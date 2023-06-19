fff081f7577b421a3a748bad0809f3a53eac59d6
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-06-19 12:07:55 +0530

    Fixed arrangments and stuff

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -5,19 +5,23 @@ import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 function VideoCard({thumbnail,tag,timestamp,title,description,gridView}) {
   return (
     <div className={`${gridView?"videocard":"videocard__col"}`}>
-        <img src={thumbnail} alt={title} className='videocard__image'/>
-        <div className={`${gridView?"videocard__tag_and_time":"videocard__tag_and_time__col"}`}>
+        <img src={thumbnail} alt={title} className={`${gridView?"videocard__image":"videocard__image__col"}`}/>
+        <div className={`${gridView?"gv1":"cv1"}`}>
+        <div className="videocard__tag_and_time">
             <div className="videocard__tag">{tag}</div>
             <div className="videocard__time">{timestamp}</div>
         </div>
 
-        <div className="videocard__title">{title}</div>
+        
+          <div className="videocard__title">{title}</div>
 
-        <div className="videocard__desc">{description}</div>
+          <div className="videocard__desc">{description}</div>
 
-        <div className="videocard__bottom">
+          <div className="videocard__bottom">
             <p>Read More </p>
             <ArrowForwardIcon/>
+          </div>
+        
         </div>
 
     </div>
